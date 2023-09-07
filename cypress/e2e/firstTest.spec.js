504b272e9e39ad2669478d133c37c4c7060fe8dc
tricky-duck <anka.matveeva@gmail.com>
2023-09-07 16:11:18 +0100

    lesson 27. Data picker logic improved

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -119,7 +119,7 @@ describe('Our first suite', () => { //open callback function
         .invoke('prop', 'value').should('contain', "Sep 18, 2023" )
 
     })
-    it('Radio buttons', () => {
+    it('radio buttons', () => {
         cy.visit('/')
         cy.contains('Forms').click()
         cy.contains('Form Layouts').click() 
@@ -141,7 +141,7 @@ describe('Our first suite', () => { //open callback function
         })
     
     })
-    it.only('check boxes', () => {
+    it('check boxes', () => {
         cy.visit('/')
         cy.contains('Modal & Overlays').click()
         cy.contains('Toastr').click() 
@@ -152,8 +152,50 @@ describe('Our first suite', () => { //open callback function
             cy.wrap(aBox).eq(1).check({force:true}).should('be.checked')
         })
     })
-    })  
 
+    it.only('datePicker improved logic', () => {
+        function selectDayFromCurrent(day){
+            let date = new Date()
+            date.setDate(date.getDate() + day)
+            let futureDay = date.getDate()
+            let futureMonth = date.toLocaleDateString('en-US', {month: 'short'})
+            let dateAssert = futureMonth+' '+futureDay+', '+date.getFullYear()    
+            cy.get('nb-calendar-navigation').invoke('attr', 'ng-reflect-date').then(dateAttr => {
+                if(!dateAttr.includes(futureMonth)){
+                    cy.get('[data-name="chevron-right"]').click()
+                    selectDayFromCurrent(day)                    
+                }
+                else {
+                    cy.get('nb-calendar-picker [class="day-cell ng-star-inserted"]').contains(futureDay).click()
+                }
+            })
+            return dateAssert
+        }
+
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains("Datepicker").click()
+
+        cy.contains('nb-card', "Common Datepicker").find('input').then(input => {
+            cy.wrap(input).click()
+            let dateAssert = selectDayFromCurrent(300)
+            cy.wrap(input).invoke('prop', 'value').should('contain', dateAssert)
+        })
+
+
+    })
+})
+      
+
+    // it('assert property', () => {
+
+
+    //     //with .then, assert the property with invoke
+    //     cy.contains('nb-card', "Common Datepicker")
+    //     .find('input').then(input => {
+    //         cy.wrap(input).click()
+    //         cy.get('nb-calendar-picker').contains('17').click()
+    //         cy.get(input).invoke('prop', 'value').should('contain', "Sep 17, 2023")
  
 // describe('Our first suite', () => { //opencallback function
 
