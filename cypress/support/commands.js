a35750fb88a7d3ac87ff3145797e90e89d51ab1a
tricky-duck <anka.matveeva@gmail.com>
2023-09-21 23:23:09 +0100

    lesson 35-47. working with API. intercept, request, route

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,5 @@
-/// <reference types="cypress" />
 // ***********************************************
-// This example commands.ts shows you how to
+// This example commands.js shows you how to
 // create various custom commands and overwrite
 // existing commands.
 //
@@ -11,31 +10,45 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
+// Cypress.Commands.add("login", (email, password) => { ... })
 //
 //
 // -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
 //
 //
 // -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
 //
 //
 // -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
 Cypress.Commands.add('openHomePage', () => {
     cy.visit('/')
+})
+
+// api headless login instead of ui, add a token to the localStorage
+Cypress.Commands.add('loginToApplication', () => {
+    
+    const userCredentials = {
+        "user": {
+            "email": Cypress.env("username"),
+            "password": Cypress.env("password")
+        }
+    }
+
+    cy.request('POST', Cypress.env('apiUrl')+'/api/users/login', userCredentials)
+        .its('body').then( body => {
+            const token = body.user.token
+            // save the token as an alias to reuse it in tests as @token
+            cy.wrap(token).as('token')
+            cy.visit('/', {
+                onBeforeLoad (win){
+                    win.localStorage.setItem('jwtToken', token)
+                }
+            })
+
+        })
+    
 })
